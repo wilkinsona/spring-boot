fba87491319a4d784fdff945b0f29397fd5aa52c
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-06-05 09:00:45 +0200

    fixup! Move Servlet high-level infrastructure to spring-boot-servlet

--- spring-boot-project/spring-boot-autoconfigure-all/src/test/java/org/springframework/boot/autoconfigure/web/servlet/MultipartAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-autoconfigure-all/src/test/java/org/springframework/boot/autoconfigure/web/servlet/MultipartAutoConfigurationTests.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.boot.actuate.web.servlet;
+package org.springframework.boot.autoconfigure.web.servlet;
 
 import java.net.URI;
 import java.util.stream.Stream;
