97b1ae4a0ea45539ba3164950d6616fd9602b5bc
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-09-23 18:31:33 +0100

    Update documentation following modularization of test-autoconfigure

--- buildSrc/src/main/java/org/springframework/boot/build/test/autoconfigure/TestSlicePlugin.java
+++ buildSrc/src/main/java/org/springframework/boot/build/test/autoconfigure/TestSlicePlugin.java
@@ -18,10 +18,13 @@ package org.springframework.boot.build.test.autoconfigure;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.attributes.Category;
+import org.gradle.api.attributes.Usage;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.plugins.PluginContainer;
 import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.TaskProvider;
 
 /**
  * {@link Plugin} for projects that define one or more test slices. When applied, it:
@@ -40,20 +43,36 @@ import org.gradle.api.tasks.SourceSet;
  */
 public class TestSlicePlugin implements Plugin<Project> {
 
+	private static final String TEST_SLICE_METADATA_CONFIGURATION_NAME = "testSliceMetadata";
+
 	@Override
 	public void apply(Project target) {
 		PluginContainer plugins = target.getPlugins();
 		plugins.apply(TestAutoConfigurationPlugin.class);
 		plugins.withType(JavaPlugin.class, (plugin) -> {
-			target.getTasks().register("generateTestSliceMetadata", TestSliceMetadata.class, (task) -> {
-				SourceSet mainSourceSet = target.getExtensions()
-					.getByType(JavaPluginExtension.class)
-					.getSourceSets()
-					.getByName(SourceSet.MAIN_SOURCE_SET_NAME);
-				task.setSourceSet(mainSourceSet);
-				task.getOutputFile().set(target.getLayout().getBuildDirectory().file("test-slice-metadata.properties"));
+			TaskProvider<GenerateTestSliceMetadata> generateTestSliceMetadata = target.getTasks()
+				.register("generateTestSliceMetadata", GenerateTestSliceMetadata.class, (task) -> {
+					SourceSet mainSourceSet = target.getExtensions()
+						.getByType(JavaPluginExtension.class)
+						.getSourceSets()
+						.getByName(SourceSet.MAIN_SOURCE_SET_NAME);
+					task.setSourceSet(mainSourceSet);
+					task.getOutputFile().set(target.getLayout().getBuildDirectory().file("test-slice-metadata.json"));
+				});
+			addMetadataArtifact(target, generateTestSliceMetadata);
+		});
+	}
+
+	private void addMetadataArtifact(Project project, TaskProvider<GenerateTestSliceMetadata> task) {
+		project.getConfigurations().consumable(TEST_SLICE_METADATA_CONFIGURATION_NAME, (configuration) -> {
+			configuration.attributes((attributes) -> {
+				attributes.attribute(Category.CATEGORY_ATTRIBUTE,
+						project.getObjects().named(Category.class, Category.DOCUMENTATION));
+				attributes.attribute(Usage.USAGE_ATTRIBUTE,
+						project.getObjects().named(Usage.class, "test-slice-metadata"));
 			});
 		});
+		project.getArtifacts().add(TEST_SLICE_METADATA_CONFIGURATION_NAME, task);
 	}
 
 }
